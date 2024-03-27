d9180f8c922496527562b1b440a3c702f015b424
Caspar <vndrks@naver.com>
2024-03-28 01:17:54 +0900

    Appt-Ex Project first commit
    .

---
+++ Source/Appointment/Appointment.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Appointment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Appointment, "Appointment" );
+
+DEFINE_LOG_CATEGORY(LogAppointment)
+ 
