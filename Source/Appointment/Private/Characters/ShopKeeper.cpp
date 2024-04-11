b950a210d1558d92971cf0c19b816037603d79b5
Caspar <vndrks@naver.com>
2024-04-11 16:28:57 +0900

    Add features for ShopKeeper

---
+++ Source/Appointment/Private/Characters/ShopKeeper.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/ShopKeeper.h"
+
+#include "Components/SkeletalMeshComponent.h"
+#include "../../AppointmentPlayerController.h"
+#include "Net/UnrealNetwork.h"
+// Sets default values
+AShopKeeper::AShopKeeper()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	ShopKeeperMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	RootComponent = ShopKeeperMesh;
+
+	bReplicates = true;
+}
+
+void AShopKeeper::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps)
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AShopKeeper, Items);
+}
+
+// Called when the game starts or when spawned
+void AShopKeeper::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShopKeeper::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AShopKeeper::Interact(AAppointmentPlayerController* PlayerController)
+{
+	if (PlayerController)
+	{
+		PlayerController->OpenShop(Items);
+	}
+	
+
+}
+
